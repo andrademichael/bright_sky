cd6abe4d91778c65d591a88c00b5f3bbd43e0000
Eric Bomblatus and Jacob Hixon <eric@>
2016-12-19 15:25:52 -0800

    Add form for zip/city/state and basic weather results page

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require('sinatra')
 require('sinatra/reloader')
 require 'pg'
 also_reload('lib/**/*.rb')
-require 'lib/location'
+# require 'lib/location'
 
 get("/") do
   erb(:index)
+end
+
+get("/weather_view") do
+  erb(:weather_view)
 end
