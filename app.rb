d7b5ec56d92fd85316bf7e1c53195bd4502440fc
Jacob Hixon and Skye Atwood <skye@>
2016-12-20 12:30:54 -0800

    add js for alerting if form is incomplete, add route to display results of first api

--- app.rb
+++ app.rb
@@ -16,12 +16,16 @@ get("/weather_view") do
   zip = params[:zip_input]
   city = params[:city_input]
   state = params[:state_input]
-  coordinates = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{city},+#{state},+#{@zip}&key=AIzaSyCejelLSq4O3hVxn7KUui2dpcc-_81XVM8")
+  coordinates = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{city},+#{state},+#{zip}&key=#{ENV['GOOGLE_GEO_KEY']}")
   geolocation_result = coordinates.fetch('results')
   latitude = geolocation_result[0].fetch('geometry').fetch('location').fetch('lat').to_f
   longitude = geolocation_result[0].fetch('geometry').fetch('location').fetch('lng').to_f
-  @city = geolocation_result[0].fetch('address_components')[1].fetch('long_name')
-  @state = geolocation_result[0].fetch('address_components')[3].fetch('short_name')
-  @location = Location.new({:name => })
+  if city == '' && state == ''
+    @city = geolocation_result[0].fetch('address_components')[1].fetch('long_name')
+    @state = geolocation_result[0].fetch('address_components')[3].fetch('short_name')
+  else
+    @city = geolocation_result[0].fetch('address_components')[0].fetch('long_name')
+    @state = geolocation_result[0].fetch('address_components')[2].fetch('short_name')
+  end
   erb(:weather_view)
 end
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('#search_form').submit(function(event) {
+    if ($('#zip_input').val() === '') {
+      if ($('#city_input').val() === '' && $('#state_input').val() === '') {
+        alert('Please enter a city and state or zipcode')
+        event.preventDefault()
+      }
+      else if ($('#city_input').val() === '' && $('#state_input').val() !== '') {
+        alert('Please enter a city')
+        event.preventDefault()
+      }
+      else if ($('#city_input').val() !== '' && $('#state_input').val() === '') {
+        alert('Please enter a state')
+        event.preventDefault()
+      }
+    }
+    else if ($('#city_input').val() === '' && $('#state_input').val() === '') {
+      if ($('#zip_input').val() === '') {
+        alert('Please enter a city and state or zipcode')
+        event.preventDefault()
+      }
+      else if ($('#zip_input').val().length() < 4) {
+        alert('Please enter a 4- or 5-character zipcode')
+        event.preventDefault()
+      }
+    }
+  });
 });
