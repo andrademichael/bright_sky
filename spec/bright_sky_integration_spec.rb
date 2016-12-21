53fc263cd8b8271890e716e26742f3d920a109fc
Michael Andrade and Skye Atwood <skye@>
2016-12-21 09:59:58 -0800

    fetch 3-day data from DarkSky object and display in weather_view

--- spec/bright_sky_integration_spec.rb
+++ spec/bright_sky_integration_spec.rb
@@ -4,8 +4,8 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("viewing current weather by zipcode", {:type => :feature}) do
-  it("allows the user to enter a zip code and see what the weather there is right now") do
+describe("viewing weather by zipcode", {:type => :feature}) do
+  it("allows the user to enter a zip code and see what the current and 3 day weather forecast is") do
     visit("/")
     fill_in("zip_input", :with => 97223)
     click_on("Check weather")
@@ -13,8 +13,8 @@ describe("viewing current weather by zipcode", {:type => :feature}) do
   end
 end
 
-describe("viewing current weather by city/state", {:type => :feature}) do
-  it("allows the user to enter a city and state and see what the weather there is right now") do
+describe("viewing weather by city/state", {:type => :feature}) do
+  it("allows the user to enter a city and state and see what the current and 3 day weather forecast is") do
     visit("/")
     fill_in("city_input", :with => 'Portland')
     fill_in("state_input", :with => 'OR')
